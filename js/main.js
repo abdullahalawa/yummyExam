38e68980744184ec9981e59eea24989e8681e261
Abdullah Alawa <awamah.group@gmail.com>
2024-04-27 22:42:19 +0300

    finish sidebar animation and content

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// sidebar menu
+let navBarBtn = $(".nav-btn").children("a");
+
+let sideBarInner = $(".innerbar").innerWidth();
+$(".sidebar").animate({ left: -sideBarInner }, 0);
+
+$(navBarBtn).on("click", function () {
+  let sideBarLeftValue = $(".sidebar").css("left");
+
+  //   Menu Close Animation
+  if (sideBarLeftValue == "0px") {
+    $(".sidebar").animate({ left: -sideBarInner }, 500);
+    navBarBtn.children("i").removeClass("fa-x");
+    navBarBtn.children("i").addClass("fa-bars");
+
+    // menu items animation in close
+    $("div.innerbar .navbar ul").children("li").animate({ top: 100 }, 200);
+  } else {
+    //   Menu open Animation
+    $(".sidebar").animate({ left: 0 }, 500);
+    navBarBtn.children("i").removeClass("fa-bars");
+    navBarBtn.children("i").addClass("fa-x");
+
+    // menu items animation in open
+    for (let i = 0; i < 5; i++) {
+      $("div.innerbar .navbar ul")
+        .children("li")
+        .eq(i)
+        .animate(
+          {
+            top: 0,
+          },
+          (i + 5) * 100
+        );
+    }
+  }
+});
